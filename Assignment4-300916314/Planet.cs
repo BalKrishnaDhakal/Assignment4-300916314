54070cdf2d476e95fcafc1984c5593df1355b361
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-21 13:11:27 -0400

    Created the Abstract Planet Class

---
+++ Assignment4-300916314/Planet.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Bal Krishna Dhakal
+ * Date: July 21, 2017
+ * Description: This is the Abstract Planet Class
+ * Version: 0.1 - Created the Abstract Planet Class
+*/
+namespace Assignment4_300916314
+{
+    /// <summary>
+    /// This is the Abstract Planet Class
+    /// </summary>
+   public abstract class Planet
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+        //pUBLIC PROPERTIES
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+      
+    }
+}
