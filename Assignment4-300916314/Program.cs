cca5eca91a019bba4bf1537f0e952a8377c40fcf
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-21 12:57:38 -0400

    Initial Commit

--- Assignment4-300916314/Program.cs
+++ Assignment4-300916314/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Bal Krishna Dhakal
+ * Date: July 13, 2017
+ * Description: Lab 24
+ * Version: 0.1 
+*/
 namespace Assignment4_300916314
 {
     class Program
