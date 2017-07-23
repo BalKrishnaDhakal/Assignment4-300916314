ee659fd78e6aef6075c20dab16b39a9d5fa06220
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-22 21:37:18 -0400

    Implemented IHasMoons and IHabitable Interfaces to TerrestrialPlanet Class

--- Assignment4-300916314/TerrestrialPlanet.cs
+++ Assignment4-300916314/TerrestrialPlanet.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
  * Date: July 22, 2017
  * Description: This is an TerrestrialPlanet Class which inherits from the Planet Class
  * and implements the IHasMoons and IHabitable Interface
- *Version:0.1 -  Created TerrestrialPlanet Class
+ *Version:0.2 -  Implemented IHasMoons and IHabitable Interfaces
  */
 namespace Assignment4_300916314
 {
@@ -20,6 +20,14 @@ namespace Assignment4_300916314
         private bool _oxygen;
         // PUBLIC PROPERTIES
         // CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor of TerrestrialPlanet Class
+        /// It takes four arguments -name(string), diameter(double), Mass(double), oxygen(bool)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="oxygen"></param>
         public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
             : base(name, diameter, mass)
         {
@@ -28,5 +36,14 @@ namespace Assignment4_300916314
         // PRIVATE METHODS
         // PUBLIC METHODS
         // PUBLIC METHODS
+        public bool HasMoons()
+        {
+            return (this.MoonCount > 0);
+        }
+        // PUBLIC METHODS
+        public bool Habitable()
+        {
+            return (this._oxygen == true);
+        }
     }
 }
