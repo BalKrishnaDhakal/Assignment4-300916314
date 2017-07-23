d27687d4fe12081e3e701b3a8ae29aa570f6cd0b
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-22 21:21:21 -0400

    Created the IHabitable Interface

---
+++ Assignment4-300916314/IHabitable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bal Krishna Dhakal
+ * Date: July 22, 2017
+ * Description: This is IHabitable Interface
+ * Version:0.1 - Created the IHabitable Interface
+ */
+namespace Assignment4_300916314
+{
+    /// <summary>
+    /// This is IHabitable Interface
+    /// </summary>
+    public interface IHabitable
+    {
+          bool Habitable();
+    }
+}
