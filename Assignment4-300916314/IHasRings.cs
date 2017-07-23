158ce206970808e522c3bd4e4df2c6191c7d3a98
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-22 20:37:54 -0400

    Created the IHasRings Interface

--- Assignment4-300916314/IHasRings.cs
+++ Assignment4-300916314/IHasRings.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text;
+/* Name: Bal Krishna Dhakal
+ * Date: July 22, 2017
+ * Description: This is IHasRings Interface
+ * Version:0.1 - Created the IHasRings Interface
+ */
 
 namespace Assignment4_300916314
 {
     public interface IHasRings
     {
+        bool HasRings();
     }
 }
