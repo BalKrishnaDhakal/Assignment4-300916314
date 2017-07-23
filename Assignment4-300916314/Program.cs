60346e5df5199dc792cd1c70d66bfb7b92755ca6
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-22 21:56:27 -0400

    Created object to the GiantPlanet Class and TerrestrialPlanet Class

--- Assignment4-300916314/Program.cs
+++ Assignment4-300916314/Program.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /* Name: Bal Krishna Dhakal
- * Date: July 13, 2017
- * Description: Lab 24
- * Version: 0.1 
+ * Date: July 22, 2017
+ * Description: Assignment 4
+ * Version: 0.1 Created object to the GiantPlanet Class and TerrestrialPlanet Class 
 */
 namespace Assignment4_300916314
 {
+    /// <summary>
+    /// This is the Program(Driver) Class
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            GiantPlanet giantPlanet = new GiantPlanet("Jupitor", 138346.5, 8888, "Gas");
+            Console.WriteLine(giantPlanet.ToString());
+            TerrestrialPlanet tereestrialPlanet = new TerrestrialPlanet("Mercury", 138346.5, 8888, true);
+            Console.WriteLine(tereestrialPlanet.ToString());
         }
     }
 }
